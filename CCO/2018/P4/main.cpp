1d57f39f8089137340cb87d80f958545b4825d0f
ismail_a <you@example.com>
2018-10-04 11:49:32 +0500

    Adding CCO 18 problem

---
+++ CCO/2018/P4/main.cpp
@@ -0,0 +1,108 @@
+/**
+*/
+/**
+SXR0aXAkI0JwbXptI3FhI3Z3I293bCNqY2IjUG0jMCNicG0jVHFkcXZvLyNCcG0jQW10bjBhY2phcWFicXZvLyNNYm16dml0MSNWdyNhdGN1am16I2tpdiNhbXF9bSNQcXUjVnd6I0F0bW14MSNQcWEjaXptI2l0dCNicHF2b2EjUXYjYnBtI3BtaWRtdmEjaXZsI3d2I21pemJwMSNFcHcjcWEjYnBtem0ja2l2I3F2Ym16a21sbSNRdiNQcWEjeHptYW12a20jbXtrbXhiI0lhI3BtI3htenVxYmJtYnBHI1BtI3N2d2VtYnAjRXBpYiMraXh4bWl6bWJwI2J3I1BxYSNrem1pYmN6bWEjSWEsI0ptbnd6bSN3eiNJbmJteiN3eiNKbXBxdmwjYnBtdTEjVnd6I2FwaXR0I2JwbXwja3d1eGlhYSNJY29wYiN3biNwcWEjc3Z3ZXRtbG9tI017a214YiNpYSNQbSNlcXR0bWJwMSNQcWEjYnB6d3ZtI2x3YnAjbXtibXZsI1dkbXojYnBtI3BtaWRtdmEjSXZsI3d2I21pemJwLyNpdmwjUG0jbm1tdG1icCNWdyNuaWJxb2NtI3F2I29jaXpscXZvI0l2bCN4em1hbXpkcXZvI2JwbXUvI053eiNQbSNxYSNicG0jVXdhYiNQcW9wMSNCcG0jQWN4em11bSMrcXYjb3R3enwsMQ==
+*/
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+
+#define F first
+#define S second
+#define endl '\n'
+#define deb(x) cout<<#x<<' '<<x<<endl;
+#define pb push_back
+
+/*
+#ifdef IZI_KATKA
+#define int __int64_t
+#else
+#define int __int64
+#endif
+*/
+
+const long long MOD = 1e9 + 7;
+const long long MAXN = 1e6 + 1;
+using namespace std;
+
+typedef long long ll;
+
+long long readInt() {
+    bool minus1 = false;
+    long long result = 0;
+    char ch;
+    ch = getchar();
+    while (true) {
+        if (ch == '-') break;
+        if (ch >= '0' && ch <= '9') break;
+        ch = getchar();
+    }
+    if (ch == '-') minus1 = true; else result = ch-'0';
+    while (true) {
+        ch = getchar();
+        if (ch < '0' || ch > '9') break;
+        result = result*10 + (ch - '0');
+    }
+    if (minus1)
+        return -result;
+    else                                             
+        return result;
+}
+
+int Q(int x, int y) {
+	cout << "? " << x << ' ' << y << endl;
+	fflush(stdout);
+	int z;
+	cin >> z;
+	return z;
+}
+
+
+int main() {
+    ll n, m, K;
+    cin >> n >> m >> K;
+	int L_x = 1, R_x = n;
+	int L_y = 1, R_y = m;
+	while (R_x - L_x > 1 || R_y - L_y > 1) {
+		int mid_l = (L_x + R_x) >> 1;
+		int mid_r = (L_y + R_y) >> 1;
+		int X = Q(mid_l, mid_r);
+		if (R_x - L_x > 1) {
+			int x1 = Q(mid_l + 1, mid_r);
+			if (X < x1) {
+				R_x = mid_l;				
+			} else {
+				L_x = mid_l;				
+			}
+		}
+		if (R_y - L_y > 1) {
+			int x1 = Q(mid_l, mid_r + 1);
+			if (X < x1) {
+				R_y= mid_r;				
+			} else {
+				L_y = mid_r;			
+			}
+		}			
+	}			
+	cout << "! " << min(min(min(Q(L_x, L_y), Q(L_x, R_y)), Q(R_x, L_y)), Q(R_x, R_y));
+    fflush(stdout);	
+    return 0;
+}
